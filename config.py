5cf5f2904cb2d6db1625093fb8ac21fb45392ef6
ThermodynamicExtraction <grouphead.spro@gmail.com>
2026-03-03 11:54:54 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# Station 42098 - Egmont Channel Entrance
+STATION_ID = "42098"
+
+# The "Frothing" Thresholds
+MIN_RIDEABLE_HEIGHT = 1.0  # ft
+LONG_PERIOD_THRESHOLD = 8.0 # sec
+WIND_CHOP_MAX_PERIOD = 4.0  # sec
+
+# Personality Settings
+STYLE = "salty_surfer"
